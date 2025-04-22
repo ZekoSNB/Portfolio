c57e41299f593ffbf682d267a5f1020104e12cc7
Maros Tomasov <maros3845@gmail.com>
2025-04-22 14:51:24 +0200

    init config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button, Input } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
