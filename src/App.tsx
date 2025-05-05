4a2fb490bbdd010192b3ea1fefd3b366e92469c0
Maros Tomasov <maros3845@gmail.com>
2025-05-05 13:31:37 +0200

    adding cursor trail

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './styles/App.css'
 import { Text, Box, Heading} from '@chakra-ui/react'
 import { FaInstagram, FaLinkedin, FaYoutube, FaGithub } from 'react-icons/fa'
-
+import { CursorTrail } from './components/CursorTrail'
 
 function App() {
 
   return (
     <>
+      <CursorTrail />
       <section style={{ backgroundColor: 'black', padding: '20px', display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
         <Heading as={"h1"} textAlign={"center"} color={'white'} size={{
           base: "4xl",
@@ -21,31 +22,31 @@ function App() {
           md: "repeat(8, 1fr)",
         }} alignItems={"center"} justifyContent={"center"} mt={4} columnGap={4} rowGap={8}>
             <a href="https://instagram.com/_ozeko" target="_blank" rel="noopener noreferrer">
-              <FaInstagram color="white" size={48} />
+              <FaInstagram className='social-icon' color="white" size={48} />
             </a>
             {/* <a href="https://www.facebook.com/MarosSNB/" target="_blank" rel="noopener noreferrer">
               <FaFacebook color="white" size={48} />
             </a> */}
             <a href="https://www.linkedin.com/in/maro%C5%A1-toma%C5%A1ov/" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin color="white" size={48} />
+              <FaLinkedin className='social-icon' color="white" size={48} />
             </a>
             <a href="https://www.youtube.com/@oZekoo" target="_blank" rel="noopener noreferrer">
-              <FaYoutube color="white" size={52} />
+              <FaYoutube className='social-icon' color="white" size={52} />
             </a>
             <a href="https://github.com/ZekoSNB" target="_blank" rel="noopener noreferrer">
-              <FaGithub color="white" size={48} />
+              <FaGithub className='social-icon' color="white" size={48} />
             </a>
             <a href="https://wakatime.com/@Zeko" target="_blank" rel="noopener noreferrer">
-              <img src="/wakatime.svg" alt='wakatime' width={48} height={48}/>
+              <img className='social-icon' src="/wakatime.svg" alt='wakatime' width={48} height={48}/>
             </a>
             <a href="https://whatpulse.org/ozeko_" target="_blank" rel="noopener noreferrer">
-              <img src="https://whatpulse.org/assets/images/logo-white.png" alt='wakatime' width={44} height={44} color='white' />
+              <img className='social-icon' src="https://whatpulse.org/assets/images/logo-white.png" alt='wakatime' width={44} height={44} color='white' />
             </a>
             <a href="https://open.spotify.com/user/maros384?si=b00cd58cd5ee4232" target="_blank" rel="noopener noreferrer">
-              <img src="/spotify.svg" alt='spotify' width={48} height={48} />
+              <img className='social-icon' src="/spotify.svg" alt='spotify' width={48} height={48} />
             </a>
             <a href="https://www.tiktok.com/@_ozeko" target="_blank" rel="noopener noreferrer">
-              <img src="/tiktok.svg" alt='spotify' width={48} height={48} />
+              <img className='social-icon' src="/tiktok.svg" alt='spotify' width={48} height={48} />
             </a>
         </Box>
       </section>
---
+++ src/components/CursorTrail.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
+
+export const CursorTrail: React.FC = () => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const springX = useSpring(mouseX, { damping: 20, stiffness: 200 });
+  const springY = useSpring(mouseY, { damping: 20, stiffness: 200 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      mouseX.set(e.clientX);
+      mouseY.set(e.clientY);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, [mouseX, mouseY]);
+
+  const style: React.CSSProperties = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '15px',
+    height: '15px',
+    borderRadius: '15%',
+    backgroundColor: '#FAF081', 
+    pointerEvents: 'none',
+    zIndex: 9999,
+  };
+
+  return (
+    <motion.div
+      style={{
+        ...style,
+        translateX: springX,
+        translateY: springY,
+      }}
+    />
+  );
+};
