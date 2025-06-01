7fd1e3c2d904de4e2a666b2f13c4c8608f7a85c0
Maros Tomasov <maros3845@gmail.com>
2025-06-02 01:22:54 +0200

    removing lead section for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import './styles/global.css';
 import { Title } from './utils/Title';
 import { CursorTrail } from './components/CursorTrail';
 import { Hero } from './pages/Hero';
-import { LeadSection } from './pages/Lead';
 
 function App() {
   Title();
@@ -12,7 +11,6 @@ function App() {
     <>
       <CursorTrail />
       <Hero />
-      <LeadSection />
     </>
   )
 }
