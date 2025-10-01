b5a307d182bad3d96634103dc81ba2ec028ae5f6
Maroš Tomašov <maros3845@gmail.com>
2025-10-01 18:55:13 +0200

    Add win and draw detection to TicTacToe game
    Introduced useEffect to check for win or draw after each move and reset the game accordingly. Added helper functions for checking win and draw conditions, and refactored code to improve clarity and maintainability.

--- client/src/pages/tabs_content/TicTacToe.tsx
+++ client/src/pages/tabs_content/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import Cross from "../../assets/tictactoe/cross.svg";
 import Circle from "../../assets/tictactoe/circle.svg";
@@ -7,6 +7,14 @@ import "../../styles/TicTacToe.css";
 
 type playFieldType = {
   [pos: number]: "O" | "X" | "N";
+};
+
+type setMoveProps = {
+  pos: number;
+  value: "X" | "O" | "N";
+  setValue: React.Dispatch<React.SetStateAction<playFieldType>>;
+  move: number;
+  setMove: React.Dispatch<React.SetStateAction<number>>;
 };
 
 function TicTacToe() {
@@ -24,6 +32,19 @@ function TicTacToe() {
     8: "N",
     9: "N",
   });
+
+  useEffect(() => {
+    if (checkWin(playField) && started) {
+      alert("Vyhrál jsi!");
+      resetGame(playField, setPlayField);
+      return;
+    }
+    if (checkDraw(playField) && started) {
+      alert("Remíza!");
+      resetGame(playField, setPlayField);
+      return; 
+    }
+  }, [playField]);
 
   if (!started) {
     return (
@@ -60,7 +81,6 @@ function TicTacToe() {
       </Box>
     );
   }
-  console.log(singlePlayer);
   return (
     <Box
       display={"grid"}
@@ -114,6 +134,7 @@ function TicTacToe() {
       {Object.entries(playField).map(([pos, value]) => {
         return (
           <Box
+            key={pos}
             display={"flex"}
             justifyContent={"center"}
             alignItems={"center"}
@@ -146,18 +167,58 @@ function TicTacToe() {
   );
 }
 
-type setMoveProps = {
-  pos: number;
-  value: "X" | "O" | "N";
-  setValue: React.Dispatch<React.SetStateAction<playFieldType>>;
-  move: number;
-  setMove: React.Dispatch<React.SetStateAction<number>>;
-};
+export default TicTacToe;
+
+function resetGame(playField: playFieldType, setPlayField: React.Dispatch<React.SetStateAction<playFieldType>>) {
+  setPlayField({
+    ...playField,
+    [1]: "N",
+    [2]: "N", 
+    [3]: "N",
+    [4]: "N",
+    [5]: "N",
+    [6]: "N",
+    [7]: "N",
+    [8]: "N",
+    [9]: "N",
+  });
+}
+
+
+
+function checkDraw(playField: playFieldType) {
+  return Object.values(playField).every(value => value !== "N");
+}
+
+
+function checkWin(playField: playFieldType) {
+  const winCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+
+  for (const combination of winCombinations) {
+    const [a, b, c] = combination;
+
+    const isNotEmpty = playField[a] !== "N" && playField[b] !== "N" && playField[c] !== "N";
+    const isWin = playField[a] && playField[a] === playField[b] && playField[a] === playField[c];
+    if (isNotEmpty && isWin) return true;
+    
+  }
+  return false;
+}
 
 function setPlayerMove(props: setMoveProps) {
   if (props.value !== "N") return;
 
   const newValue = props.move % 2 === 0 ? "X" : "O";
+
   props.setValue((prev) => ({
     ...prev,
     [props.pos]: newValue,
@@ -184,4 +245,3 @@ function renderImg(pos: string, value: string, move: number) {
   );
 }
 
-export default TicTacToe;
